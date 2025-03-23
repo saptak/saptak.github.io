d786399720fbb61e57339dffb18a6087740f9152
Saptak Sen <saptak@gmail.com>
2025-03-23 07:11:12 -0700

    Fix jekyll-serve.sh to build all posts by default

--- jekyll-serve.sh
+++ jekyll-serve.sh
@@ -2,10 +2,12 @@
 
 set -e
 
-LIMIT_POSTS="${LIMIT_POSTS:-5}"
+LIMIT_POSTS="${LIMIT_POSTS:-0}"
 
 if [[ "$LIMIT_POSTS" -gt 0 ]]; then
   echo -e "\033[1;33m[WARN] Telling jekyll to only generate the last $LIMIT_POSTS posts. To change this limit, set the LIMIT_POSTS environment variable.\033[0m"
+  bundle exec jekyll serve --drafts --limit_posts "$LIMIT_POSTS"
+else
+  echo -e "\033[1;32m[INFO] Building all posts. Set LIMIT_POSTS environment variable to limit the number of posts generated.\033[0m"
+  bundle exec jekyll serve --drafts
 fi
-
-bundle exec jekyll serve --drafts --limit_posts "$LIMIT_POSTS"
